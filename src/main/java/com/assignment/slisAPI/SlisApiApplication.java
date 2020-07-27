7109034eae99b27ce9b88a50322e09b581502be3
jpbinith <jpbinith@gmail.com>
2020-07-27 23:56:13 +0530

    initial commit

---
+++ src/main/java/com/assignment/slisAPI/SlisApiApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.slisAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlisApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlisApiApplication.class, args);
+	}
+
+}
